fd103e5afafb0653f4cf039f190055f04c3a021a
haquavis <176657236+haquavis@users.noreply.github.com>
2024-07-27 10:04:20 +1000

    Created and wrote basic input/output code for the guessing game
    The user is now prompted to input a value that will be compared to the computers auto-generated number at a later stage in the project

---
+++ guessing_game/target/debug/deps/guessing_game-2aff2e55542e0262.d
@@ -0,0 +1,5 @@
+/home/haquavis/learning/rust/beginner/tutorials/the-book/guessing_game/target/debug/deps/guessing_game-2aff2e55542e0262: src/main.rs
+
+/home/haquavis/learning/rust/beginner/tutorials/the-book/guessing_game/target/debug/deps/guessing_game-2aff2e55542e0262.d: src/main.rs
+
+src/main.rs:
---
+++ guessing_game/target/debug/guessing_game.d
@@ -0,0 +1,1 @@
+/home/haquavis/learning/rust/beginner/tutorials/the-book/guessing_game/target/debug/guessing_game: /home/haquavis/learning/rust/beginner/tutorials/the-book/guessing_game/src/main.rs
